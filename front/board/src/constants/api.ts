6c183956ced5a087f386d11bfa1e78d5403c0d7d
cheolmin99 <cjfals8570@naver.com>
2023-03-31 15:49:05 +0900

    2023.03.31 15:50

--- front/board/src/constants/api.ts
+++ front/board/src/constants/api.ts
@@ -8,10 +8,10 @@ export const mutipartHeader = () => {
 const HOST = 'http://localhost:4040/'
 
 export const SIGN_UP_URL = `${HOST}auth/sign-up`;
-
 export const SIGN_IN_URL = `${HOST}auth/sign-in`;
 
 export const GET_USER_URL = `${HOST}api/user/`;
+export const PATCH_PROFILE_URL = `${HOST}api/user/profile`;
 
 export const GET_LIST_URL = `${HOST}api/board/list`;
 export const GET_MY_LIST_URL = `${HOST}api/board/my-list`;
